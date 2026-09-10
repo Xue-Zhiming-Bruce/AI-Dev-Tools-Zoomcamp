36519def05e916a9dc3e1124e26fbe5995b5940f
Zhiming Xue <zhiming.xue.2024@mitb.smu.edu.sg>
2026-09-11 00:24:53 +0800

    Remove Homework1 working copy (kept in git history)

--- Homework1/chore/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ChoreConfig(AppConfig):
-    name = "chore"
--- Homework1/chore/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("chore/<int:chore_id>/done/", views.mark_done, name="mark_done"),
-    path("chore/<int:chore_id>/snooze/<str:delta>/", views.snooze, name="snooze"),
-]
